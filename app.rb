8b5434db397c588073e5ea477a9448395c3a61a2
Celine Anyaegbunam <cea03@icloud.com>
2018-07-17 13:47:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "It was so awesome to see Karlie yesterday! She put a lot of our pictures and videos on her insta! I love her so much! She is so nice!"
   end
+
+get '/kitty'do
+  "Hello Kitty"
+end 
 
 end
